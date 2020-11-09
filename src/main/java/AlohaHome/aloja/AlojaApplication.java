bb4530ad8d2a8f7dd2b2b3d83367d0f9c6120b87
brayan115 <brayan.blas@tecsup.edu.pe>
2020-11-09 17:40:54 -0500

    first commita

---
+++ src/main/java/AlohaHome/aloja/AlojaApplication.java
@@ -0,0 +1,13 @@
+package AlohaHome.aloja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlojaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AlohaHome/aloja/AlojaApplicationTests.java
@@ -0,0 +1,13 @@
+package AlohaHome.aloja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
